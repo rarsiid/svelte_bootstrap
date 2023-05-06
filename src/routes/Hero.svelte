c004eafb57bc67c428b9329254a2cff5883ffa95
rarsiid@gmail.com <rarsiid@gmail.com>
2023-05-06 19:20:01 +0700

    Tutor part 3

---
+++ src/routes/Hero.svelte
@@ -0,0 +1,12 @@
+<div> class="hero">
+<h2>Busines name tag line</h2>
+<p>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga aspernatur rerum similique cupiditate amet ex. Dolor impedit error magni corrupti mollitia. Veritatis praesentium, enim nemo distinctio corrupti qui soluta vitae.
+</p>
+</div>
+
+<style>
+    .hero {
+        border-color: cornflowerblue;
+    }
+</style>
